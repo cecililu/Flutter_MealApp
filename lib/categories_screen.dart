94705ead101d34521017ca9aa821f544b37eddfc
cecil <cecilghimire@gmail.com>
2023-01-06 10:22:48 +0545

    added category screens and categoryclas

---
+++ lib/categories_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class Categories_screen extends StatefulWidget {
+  const Categories_screen({super.key});
+
+  @override
+  State<Categories_screen> createState() => _Categories_screenState();
+}
+
+class _Categories_screenState extends State<Categories_screen> {
+  @override
+  Widget build(BuildContext context) {
+    return GridView(
+      gridDelegate: SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20),
+      children: [
+        Text('Grid'),
+      ],
+    );
+  }
+}
