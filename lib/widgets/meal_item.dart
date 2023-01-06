67bfa90ffead8faf67a30dee1b8e0e8fc21811be
cecil <cecilghimire@gmail.com>
2023-01-06 21:39:39 +0545

    list view of Category Page

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -18,6 +18,43 @@ class MealItem extends StatelessWidget {
       required this.complexity,
       required this.affordibility});
   void selectMeal() {}
+  String get complexityText{
+   
+   switch(complexity){
+        case Complexity.Simple:
+          return "Simple" ;
+          break;
+        case Complexity.Hard:
+          return "Hard" ;
+          break;
+        case Complexity.Challenging:
+          return "Challanging" ;
+          break;
+        default:
+          return 'Unknown';
+
+  
+   }
+  }
+   String get cost{
+   
+   switch(affordibility){
+
+        case Affordability.Affordable:
+          return "Affordable" ;
+          break;
+        case Affordability.Luxurious:
+          return "Luxorious" ;
+          break;
+        case Affordability.Pricey :
+          return "Pricey" ;
+          break;
+        default:
+          return 'Unknown';
+
+  
+   }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -41,8 +78,51 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
+                ),
+                Positioned(
+                  bottom: 20,
+                  right: 10,
+                  
+                  child: Container(
+                    padding: EdgeInsets.all(8),
+                    color: Colors.black54,
+                    width: 220,
+                    child: 
+                      Text(
+                        title,
+                        style: TextStyle(fontSize: 25, color: Colors.white,fontWeight: FontWeight.w600,),
+                        softWrap: true,
+                        overflow: TextOverflow.fade,
+                      ),
+                    
+                  ),
                 )
               ],
+            ),
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Row(children: [
+                    Icon(Icons.schedule),
+                    SizedBox(width: 20,),
+                    Text(duration.toString()+' mins'),
+
+                  ],),
+                  Row(children: [
+                    Icon(Icons.cases_outlined),
+                    SizedBox(width: 20,),
+                    Text(complexityText.toString()),
+                    
+                  ],),
+                  Row(children: [
+                    Icon(Icons.monetization_on),
+                    SizedBox(width: 10,),
+                    Text(cost.toString()),
+                    
+                  ],)
+                ],),
             )
           ],
         ),
