c666e3848842ec43f5c0c7f5e3403e6476ff6b70
cecil <cecilghimire@gmail.com>
2023-01-06 14:21:30 +0545

    added navigator to category item

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,23 +1,38 @@
-import 'dart:ui';
+
 
 import 'package:flutter/material.dart';
+import 'package:mealer/category_meal_screens.dart';
 
 class Categoryitem extends StatelessWidget {
   final Color color;
   final String title;
   const Categoryitem({super.key, required this.title, required this.color});
+  
+  void selectCategory(context){
+      Navigator.of(context).push(MaterialPageRoute(builder: (_){
+         return CatergoryMealSceeen();
+      }));
+      
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-        padding: const EdgeInsets.all(15),
-        child: Text(title,style:Theme.of(context).textTheme.headline1,),
-        decoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [color.withOpacity(0.7), color],
-              begin: Alignment.topLeft,
-              end: Alignment.topCenter),
-          borderRadius: BorderRadius.circular(12),
-        ));
+    return InkWell(
+      onTap: ()=>selectCategory(context),
+      splashColor: Colors.red,
+      borderRadius: BorderRadius.circular(15),
+      child:
+        Container(    
+            padding: const EdgeInsets.all(15),
+            child: Text(title,style:Theme.of(context).textTheme.headline1,),
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [color.withOpacity(0.7), color],
+                  begin: Alignment.topLeft,
+                  end: Alignment.topCenter),
+              borderRadius: BorderRadius.circular(12),
+            )),
+    
+    );
   }
 }
