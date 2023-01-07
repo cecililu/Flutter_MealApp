85902c3beeba70f37a885dbd99efff4abf3ca430
cecil <cecilghimire@gmail.com>
2023-01-07 15:13:11 +0545

    added bootom navigation bar

--- lib/screen/tab_screen.dart
+++ lib/screen/tab_screen.dart
@@ -11,28 +11,43 @@ class TabScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabScreen> {
+  List <Widget>_pages=[
+Categories_screen(),FavouriteScreen()
+  ];
+ int _selectedPageIndex=0;
+  void _selectPage(int index){
+    setState(() {
+      _selectedPageIndex=index;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    return DefaultTabController(
-    length: 2, 
-    child: Scaffold(
-      
+    return  Scaffold(
       appBar: AppBar(
               title: Text('Meal App'), 
-              bottom: TabBar(
-                tabs:[
-                 Tab(icon: Icon(Icons.category),text: 'Categories'),
-                  Tab(icon: Icon(Icons.star),text: 'Favourites'),
-                 ],
+              // bottom: TabBar(
+              //   unselectedLabelColor: Color.fromARGB(255, 26, 151, 32),
+              //   // tabs:[
+              //   //  Tab(icon: Icon(Icons.category),text: 'Categories'),
+              //   //   Tab(icon: Icon(Icons.star),text: 'Favourites'),
+              //   //  ],
              
-             ),
-
-      ),
-      body:  TabBarView(
-        children: [Categories_screen(),FavouriteScreen()]
-      ),
-    )
+             ),   
+      body: _pages[_selectedPageIndex],
+      bottomNavigationBar:BottomNavigationBar(
+        onTap: _selectPage,
+        backgroundColor: Colors.red,
+        items: const[
+            BottomNavigationBarItem(
+              icon: Icon(Icons.category),
+              label: "Main"
+              ),
+         BottomNavigationBarItem(
+          icon: Icon(Icons.star),
+          label: "Favourite"
+          ),
+        ]
+        ) ,  
     );
-    
   }
 }
